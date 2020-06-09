70aa39981c709ca47cda5aa62a83a2e4bb80d9d3
lk <18337180580@163.com>
2020-06-09 14:44:03 +0800

    menus

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 require_once './vendor/autoload.php';
 initTools();
 
+
 $res= getPublicPath(true);
 //dump($res);
 
--- src/public/html/menus.php
+++ src/public/html/menus.php
@@ -2,7 +2,6 @@
 $path =getPublicPath(false);
 $menus = isset($menus)&&$menus?$menus:(new \admintools\tools\tools\Html())->menus;
 ?>
-<link rel="stylesheet" href="<?php echo $path; ?>layui/css/layui.css">
 <link rel="stylesheet" href="<?php echo $path; ?>css/asidenav.css">
 <script>
     !window.jQuery && document.write('<script src="<?php echo $path; ?>js/jquery.min.js"><\/script>');
@@ -12,13 +11,20 @@ $menus = isset($menus)&&$menus?$menus:(new \admintools\tools\tools\Html())->menu
     !window.layer && document.write('<script src="<?php echo $path; ?>layui/layui.all.js"><\/script>');
 </script>
 
-<div class="aside-nav bounceInUp animated" id="aside-nav">
-    <label for="" class="aside-menu" title="Admin Tools">Tools</label>
+<div  class="aside-nav bounceInUp animated" id="aside-nav">
+    <label  class="aside-menu" title="Admin Tools">Tools</label>
     <?php foreach ($menus as $menu): ?>
         <a _link="<?php echo $menu['link']; ?>" href="javascript:void(0)" title="<?php echo $menu['title']; ?>" class="tool_menu menu-item <?php echo $menu['class']; ?> "><?php echo $menu['title']; ?></a>
     <?php endforeach; ?>
 </div>
 <script>
+    $(function () {
+        $(".aside-menu").each(function (index,val) {
+            if(index!=0){
+                $(val).parent().remove();
+            }
+        })
+    });
     $(document).on('click','.tool_menu',function () {
         var url =$(this).attr('_link');
         layer.open({
